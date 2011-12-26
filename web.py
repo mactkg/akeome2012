6523be13d1ec60de4eaf1a79986fbba0f15043b0
mactkg <mactkg@gmail.com>
2011-12-27 00:18:54 +0900

    add more hint

--- web.py
+++ web.py
@@ -21,6 +21,9 @@ def reply():
     if request.form['answer'] == u"こたつ":
         flash(u'ok')
         return render_template('ans.html', messages=u"せいかーい。")
+    elif request.form['answer'] == u"ごたつ":
+        flash(u'おしい。うさぎちゃんが求めているのは「冬を超すためにたいせつなもの」だー！')
+        return redirect(url_for('index'))
     else:
         flash(u'ヒント：今年の干支と数にちゅうもくだー')
         return redirect(url_for('index'))
