c0e56f4538eef607005f60098cc23e8d8e99cd0b
mactkg <mactkg@gmail.com>
2011-12-30 02:12:00 +0900

    remove from function and add 404 function

--- web.py
+++ web.py
@@ -33,13 +33,10 @@ def reply():
 def omake():
     return render_template('omake.html')
 
-@app.route('/from')
-def fromReturn():
-    s = ""
-    for i in request.headers:
-        s = s + "," + str(i)
-    return s
+@app.errorhandler(404)
+def not_found(error):
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
