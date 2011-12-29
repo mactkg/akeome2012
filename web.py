599fd58069304c387d872e0bb9a9262056da4457
mactkg <mactkg@gmail.com>
2011-12-30 00:12:59 +0900

    flask test

--- web.py
+++ web.py
@@ -35,7 +35,7 @@ def omake():
 
 @app.route('/from')
 def fromReturn():
-    return os.getenv('HTTP_REFERER')
+    return request.environ
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
